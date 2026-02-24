ed86c002c930e2b93647bf7d9087c1bc0c6bc644
Secret Commander of Jesus of Nazareth: Reyo / Reynald R. <reynaldramirez@hotmail.com>
2026-02-23 22:17:40 -0400

    [U] Does this offend you? .. ['John 6']

---
+++ private_includes/conexion.php
@@ -0,0 +1,6 @@
+<?php
+## Conexion to be placed here after 2 hours.. / 4th of the time that an elephants gives birth.
+## You shall enter, but slowly; In order that the ANIMALS do not come against you. - Jesus Christ of Nazareth.
+
+
+?>
